0838aa312e84b34e4b1476c1e2ef7c2c9e112895
Dan Lindsay <dlindsay72@hotmail.com>
2017-03-20 13:23:34 -0400

    UI setup for LoginVC done, actions and outlets created

---
+++ TheStore/LoginVC.swift
@@ -0,0 +1,55 @@
+//
+//  LoginVC.swift
+//  TheStore
+//
+//  Created by Dan Lindsay on 2017-03-20.
+//  Copyright © 2017 Dan Lindsay. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    
+    // MARK: IBOutlets
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    // MARK: - IBActions
+    
+    
+    @IBAction func didTapCancel(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    
+    @IBAction func didTapSignIn(_ sender: MyButton) {
+        
+    }
+    
+    
+
+}
