e2d789216fa2ddf2891769d22f173b43d4b44d33
Dan Lindsay <dlindsay72@hotmail.com>
2017-03-22 13:13:57 -0400

    ReviewOrderTableVC created, UI created with constraints and custom cell classes for Payment and Shipping created as well

---
+++ TheStore/PaymentTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PaymentTableViewCell.swift
+//  TheStore
+//
+//  Created by Dan Lindsay on 2017-03-22.
+//  Copyright © 2017 Dan Lindsay. All rights reserved.
+//
+
+import UIKit
+
+class PaymentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
